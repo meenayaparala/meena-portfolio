375c09050d56656e339e6d932ee714714652cee7
meenayaparala <meenayaparala@gmail.com>
2025-05-05 17:16:36 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/meena-portfolio/', // Ensure this matches your repository name
+  base: '/', // Use '/' for Netlify
   plugins: [react()],
 });
